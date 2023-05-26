9a375322c7f78531ca4b2fc1952b1a05c90a46e3
Nickkolass <nickkolass@DESKTOP-FHEINCR.localdomain>
2023-05-26 18:25:07 +0400

    =genders viewing update, added ability to change roles for users, ui:auth register aligned with user::store  assigned unique fields in migrations, up to php 8.1, node 18.16

--- app/Components/ImportDataClient.php
+++ app/Components/ImportDataClient.php
@@ -4,19 +4,19 @@ namespace App\Components;
 
 use GuzzleHttp\Client;
 
-class ImportDataClient 
+class ImportDataClient
 {
     public $client;
 
     /**
      * ImportDataClient constructor.
-     * @param $client 
+     * @param $client
      */
-    
+
     public function __construct()
     {
         $this->client = new Client([
-            'base_uri' => '172.31.80.1:8876',
+            'base_uri' => '172.28.64.1:8876',
             'timeout' => 2.0,
             'verify' => false,
         ]);
