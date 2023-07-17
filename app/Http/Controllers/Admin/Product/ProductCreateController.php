b46b4338a22ef6021a216db8e371d3a75b124b71
Nickkolass <kajib9lh@rambler.ru>
2023-07-17 11:10:00 +0400

    small upd

--- app/Http/Controllers/Admin/Product/ProductCreateController.php
+++ app/Http/Controllers/Admin/Product/ProductCreateController.php
@@ -44,7 +44,7 @@ class ProductCreateController extends Controller
     public function types(): View
     {
         //в случае ошибки валидации редирект на пост роут невозможен, поэтому гет с проверкой внутри метода
-        session()->has('create') & request()->has('propertyValues') & request()->has('optionValues') ?: abort(404);
+        if(app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName() == 'admin.products.store') abort(404);
 
         session(['create.propertyValues' => array_filter(request('propertyValues'))]);
 
